b16d23507cbe6c5d6221b9710744fbb05ee72010
Delyassss <delyassss@student.1337.ma>
2026-03-11 21:37:52 +0000

    ex 00 probably done

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,11 +1,29 @@
 #include "BitcoinExchange.hpp"
+
+std::string &trimWithSpaces(std::string &str)
+{
+    str.erase(0, str.find_first_not_of(" \t\r"));
+    str.erase(str.find_last_not_of(" \t\r") + 1);
+    return (str);
+}
 bool isNumber(std::string line, int forDate)
 {
-    for (size_t i = 0; i < line.length(); i++)
+    size_t i = 0;
+    
+    // std::cout << "Line {" << line << "}" << std::endl;
+
+    if (line.empty())
+        return false;
+
+     if (line[i] == '-' || line[i] == '+')
+        i++;
+
+    for (; i < line.length(); i++)
     {
-        if (!std::isdigit(line[i]))
+        if ((!std::isdigit(line[i]) && line[i] != '.'))
             return (std::cout << "Error: not a valid number." << std::endl, false);
     }
+
     if (std::atof(line.c_str()) < 0)
         return (std::cout << "Error: not a positive number." << std::endl, false);
     if (!forDate && std::atof(line.c_str()) > 1000)
@@ -25,7 +43,7 @@ bool isleap (int year)
 bool checkDay(long day, long month, long year)
 {
     bool isLeap = isleap(year);
-    int fevrier = isleap ? 29 : 28;
+    int fevrier = isLeap ? 29 : 28;
     int  dayinmonth[12] = {31, fevrier, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if (day < 1 || day > dayinmonth[month - 1])
         return (std::cout << "Error: not a valid day." << std::endl, false);
@@ -34,27 +52,28 @@ bool checkDay(long day, long month, long year)
 
 bool validDate(std::vector<std::string> strs)
 {
-    long year = atol(strs[0].c_str());
-    long month = atol(strs[1].c_str());
-    long day = atol(strs[2].c_str());
+    long year = std::atol(strs[0].c_str());
+    long month = std::atol(strs[1].c_str());
+    long day = std::atol(strs[2].c_str());
     if (year < 2009 || year > 2022)
         return (std::cout << "Error: not a valid date." << std::endl, false);
     if (month < 1 || month > 12 || day < 1 || day > 31)
         return (std::cout << "Error: not a valid date." << std::endl, false);
     return  (checkDay(day, month, year));
 }
+
 bool isTime(std::string line)
 {
     std::vector <std::string> strs;
-    std::stringstream ss(line);
     std::string item;
     
     if (line.size() != 10)
-        return (std::cout << "Error: invalid date format" << std::endl, false);
+    return (std::cout << "Error: invalid date format" << std::endl, false);
+    std::stringstream ss(line);
 
     while (std::getline(ss, item , '-'))
     {
-        if (item.empty() || !isNumber(item, true))
+        if (item.empty() || !isNumber(item, 1))
             return (std::cout << "Error: invalid date format" << std::endl, false);
         strs.push_back(item);
     }
@@ -64,34 +83,59 @@ bool isTime(std::string line)
     return (true);
 }
 
-bool checkFormatandInsert(std::string line, char split, int insert , data_M &data)// 1 for insert
+bool BitcoinExchange::checkFormatandInsert(std::string line, char delim, int insert , data_M &data)// 1 for insert
 {
     std::vector <std::string> strs;
+    std::string extracted;
     size_t pos = 0;
-    while ((pos = line.find(split, pos)) != std::string::npos)
+    while ((pos = line.find(delim, pos)) != std::string::npos)
     {
-        strs.push_back(line.substr(0, pos));
-        line.erase(0, pos + 1);
-    }
-        if (strs.size() > 2)
-        {
-            return (std::cout  << "Error: too many arguments" << std::endl, false);
-        }
+        extracted = line.substr(0, pos);
+        // std::cout << "extracted {" << extracted << "}" << std::endl;
 
-    if (isTime(strs[0]) && isNumber(strs[1], insert))
+        strs.push_back(extracted);
+        line.erase(0, pos + 1);
+        pos = 0;
+    }
+    if (line.size() > 0)
     {
-        std::pair<std::string, float> p(strs[0], std::atof(strs[1].c_str()));
+        extracted = line.substr(0 , pos -3);
+        // std::cout << "extracted {" << extracted << "}" << std::endl;
+        strs.push_back(extracted);
+    }
+
+    if (strs.size() != 2)
+    {
+        return (std::cout  << "Error: Invalid arguments" << std::endl, false);
+    }
+    std::string date = strs[0];
+    date = trimWithSpaces(date);
+    std::string value = strs[1];
+    value = trimWithSpaces(value);
+
+
+    // std::cout << "value {" << value << "}" << std::endl;
+    // std::cout << "date {" << date << "}" << std::endl;
+
+    if (isTime(date) && isNumber(value, insert))
+    {
+        std::pair<std::string, double> p(date, std::atof(value.c_str()));
         if (insert == 1)
             data.insert(p);
+        else
+            return (calcul(date,value, data));
+            
         return (true);
     }
+    return (false);
 }
-void BitcoinExchange::readData(std::map<std::string, float> &mydata, std::string filename, char delim, int insert)// 1 for inserting to the map
+
+void BitcoinExchange::readData(std::map<std::string, double> &mydata, std::string filename, char delim, int insert)// 1 for inserting to the map
 {
     if (filename.empty())
         (throw std::runtime_error("Empty filename"));
 
-    std::ifstream d_input(filename);
+    std::ifstream d_input(filename.c_str());
     if (!d_input.is_open())
         (throw std::runtime_error("Error opening file"));
 
@@ -99,31 +143,53 @@ void BitcoinExchange::readData(std::map<std::string, float> &mydata, std::string
     if (delim == ',')
     {
         std::getline(d_input, line);
-        if (line != "date,exchange_rate")
+        if (line != "date,exchange_rate" && line.find("date,exchange_rate") == std::string::npos)
              throw (std::runtime_error("Error: invalid database format."));
     }
     else if (delim == '|')
     {
         std::getline(d_input, line);
-        if (line != "date|value")
+        if (line != "date | value" && line.find("date | value") == std::string::npos)
             std::cerr << "Error: missing (date | value) header !" << std::endl;
     }
-    while (!line.empty())
+    while (std::getline(d_input, line))
     {
-        if (!checkFormatandInsert(line, delim, insert, mydata) && insert)
-            throw (std::runtime_error("Error: invalid database format."));
-            std::getline(d_input, line);
+        if (d_input.eof())
+            break;           
+        if (checkFormatandInsert(line, delim, insert, mydata) == false)
+        {
+           if (delim == ',')
+           {
+                d_input.close();
+                throw (std::runtime_error("Error: invalid database format."));
+           }
+        }
+        //std::getline(d_input, line);
     }
+    d_input.clear();
+    d_input.close();
     return ;
 }
+
 BitcoinExchange::BitcoinExchange(std::string filename)
 {
-    readData(_data, "data.csv", ',', 1);
-    readData(_input, filename, '|' , 0);
+    readData(getdata(), "data.csv", ',', 1);
+    readData(getdata(), filename, '|' , 0);
+}
 
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src)
+{
+    *this = src;
+}
 
-
-
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
+{
+    if (this != &rhs)
+    {
+        this->_data = rhs._data;
+        this->_input = rhs._input;
+    }
+    return (*this);
 }
 
 data_M &BitcoinExchange::getdata()
@@ -138,5 +204,45 @@ input_M &BitcoinExchange::getinput()
 
 BitcoinExchange::~BitcoinExchange()
 {
+
+}
+
+void printRes(double INP_value, double DB_value, std::string date)
+{
+    double Res = INP_value * DB_value;
+    std::cout << date << " => " << Res << std::endl;
+}
+
+bool BitcoinExchange::calcul(std::string date, std::string svalue, data_M &data)
+{
+    char *endptr;
+
+    double value = std::strtod(svalue.c_str(), &endptr);
+    if (endptr == svalue.c_str())
+        return (std::cout << "Error: not a valid number." << std::endl, false);
+    if (value < 0)
+        return (std::cout << "Error: not a valid number." << std::endl, false);
+    if (value > 1000)
+        return (std::cout << "Error: too large a number." << std::endl, false);
+    if (data.empty())
+        return (std::cout << "Error: database is empty" << std::endl, false);
+
+    data_M::iterator it = data.lower_bound(date);
+    
+        if ( it != data.end() && it->first == date)
+        {
+            printRes(value, it->second, it->first);
+            return (true);
+        }
+         else
+        {
+            if (it != data.begin())
+                 it--;
+            else
+                return (std::cout << "Error: Bad Input !", false);
+             printRes(value, it->second, it->first);
+             return (true);
+         }     
+    return (false);
 
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -7,6 +7,8 @@
 #include "fstream"
 #include "vector"
 #include "sstream"
+#include <cstdlib>
+#include <string>
 
 struct inputfile
 {
@@ -14,7 +16,7 @@ struct inputfile
     void    *value;
 };
 
-typedef std::map<std::string, float> data_M;
+typedef std::map<std::string, double> data_M;
 typedef std::map<std::string, float> input_M;
 
 class BitcoinExchange
@@ -29,9 +31,11 @@ class BitcoinExchange
     ~BitcoinExchange();
     BitcoinExchange(BitcoinExchange const &src);
     BitcoinExchange &operator=(BitcoinExchange const &rhs);
-    data_M &getdata();
-    input_M &getinput();
-    void readData(std::map<std::string, float> &mydata, std::string filename, char delim, int insert);
+    data_M      &getdata();
+    input_M     &getinput();
+    void        readData(std::map<std::string, double> &mydata, std::string filename, char delim, int insert);
+    bool        calcul(std::string date , std::string value, data_M &data);
+    bool        checkFormatandInsert(std::string line, char split, int insert , data_M &data);
     
 
 
